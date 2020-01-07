75252f405c58c1216cdd9cf3fed85777f7191a9c
anawat@opendream.co.th <anawat@opendream.co.th>
2020-01-07 15:57:48 +0700

    ex3.1

---
+++ ex3/ex3.1.go
@@ -0,0 +1,24 @@
+package main
+
+import ("fmt"
+		"os"
+		"strconv"
+)
+
+func main() {
+	arg := os.Args[1]
+
+	n, err := strconv.Atoi(arg)
+	if err == nil {
+		//fmt.Println(n)
+		for i:=1;i<=n;i++{
+			for j:=1;j<=i;j++{
+				fmt.Print("*")
+			}
+			fmt.Print("\n")
+		}
+	}else{
+		fmt.Println("argument error")
+	}
+	
+}
